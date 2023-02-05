14b6e4bc25826c680ddba569c9a4d14e1c5512e8
drathier <github@fi.fo>
2023-02-05 17:51:57 +0100

    Update tag

--- src/Language/PureScript/Make/Actions.hs
+++ src/Language/PureScript/Make/Actions.hs
@@ -340,7 +340,7 @@ buildMakeActions outputDir filePathMap foreigns usePrefix mExternsMemCache =
   requiresForeign = not . null . CF.moduleForeign
 
   progress :: ProgressMessage -> Make ()
-  progress = liftIO . TIO.hPutStr stderr . (<> "\n") . renderProgressMessage "Compiling V15 "
+  progress = liftIO . TIO.hPutStr stderr . (<> "\n") . renderProgressMessage "Compiling V16 "
 
   readCacheDb :: Make CacheDb
   readCacheDb = readCacheDb' outputDir
