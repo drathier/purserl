946fc86a2f41b6013a32fe823afbfab90bf1e299
drathier <github@fi.fo>
2023-10-03 02:41:30 +0200

    Print cycle instead of scc on cyclic imports, and try to find the shortest cycle if it's easy to find.

--- src/Language/PureScript/ModuleDependencies.hs
+++ src/Language/PureScript/ModuleDependencies.hs
@@ -10,13 +10,17 @@ module Language.PureScript.ModuleDependencies
 import Protolude hiding (head)
 
 import Data.Array ((!))
-import Data.Graph (SCC(..), graphFromEdges, reachable, stronglyConnComp)
+import Data.Maybe (fromJust)
+import Data.List qualified as List
+import Data.Graph (SCC(..), graphFromEdges, reachable, stronglyConnComp, dfs, Vertex, edges)
 import Data.Set qualified as S
 import Language.PureScript.AST (Declaration(..), ErrorMessageHint(..), Module(..), SourceSpan)
 import Language.PureScript.Constants.Prim qualified as C
 import Language.PureScript.Crash (internalError)
 import Language.PureScript.Errors (MultipleErrors, SimpleErrorMessage(..), addHint, errorMessage', errorMessage'', parU)
 import Language.PureScript.Names (ModuleName)
+import Data.List.NonEmpty qualified as NE
+import Data.Sequence qualified as Seq
 
 -- | A list of modules with their transitive dependencies
 type ModuleGraph = [(ModuleName, [ModuleName])]
@@ -45,7 +49,7 @@ sortModules dependencyDepth toSig ms = do
       ms' = (\m -> (m, toSig m)) <$> ms
       mns = S.fromList $ map (sigModuleName . snd) ms'
     verts <- parU ms' (toGraphNode mns)
-    ms'' <- parU (stronglyConnComp verts) toModule
+    ms'' <- parU (stronglyConnComp verts) (toModule verts)
     let (graph, fromVertex, toVertex) = graphFromEdges verts
         moduleGraph = do (_, mn, _) <- verts
                          let v       = fromMaybe (internalError "sortModules: vertex not found") (toVertex mn)
@@ -74,16 +78,56 @@ usedModules (ImportDeclaration (ss, _) mn _ _) = pure (mn, ss)
 usedModules _ = Nothing
 
 -- | Convert a strongly connected component of the module graph to a module
-toModule :: MonadError MultipleErrors m => SCC (a, ModuleSignature) -> m (a, ModuleSignature)
-toModule (AcyclicSCC m) = return m
-toModule (CyclicSCC ms) =
+toModule :: MonadError MultipleErrors m => [((a, ModuleSignature), ModuleName, [ModuleName])] -> SCC (a, ModuleSignature) -> m (a, ModuleSignature)
+toModule _ (AcyclicSCC m) = return m
+toModule verts (CyclicSCC ms) =
   case nonEmpty ms of
     Nothing ->
       internalError "toModule: empty CyclicSCC"
-    Just ms' ->
+    Just ms'@(msRoot :| msRest) ->
+      let shortestCycle n =
+            let (graph, fromVertex, toVertex) = graphFromEdges verts in
+            let msToVertex a = fromJust (toVertex (sigModuleName (snd a))) in
+            let f z = sigModuleName <$> (\(x,_,_) -> snd x) $ fromVertex z in
+            let (sccNodes :: S.Set Vertex) = S.fromList $ fromJust <$> toVertex <$> sigModuleName <$> snd <$> (msRoot : msRest) in
+            let startState = Seq.fromList $ filter (\(_,_,x) -> S.member x sccNodes) $ NE.toList $ (\x -> (S.empty, [], msToVertex x)) <$> ms' in
+
+            let firstNode = fromJust (toVertex (sigModuleName (snd n))) in
+            let dfs :: S.Set Vertex -> [Vertex] -> [Vertex]
+                dfs seen (a:ax) | S.member a sccNodes == False = dfs seen ax
+                dfs seen (a:ax) | S.member a seen = [a]
+                dfs seen (a:_) = a : dfs (S.insert a seen) (graph ! a)
+
+                cycleWithTail@(cycleWithTailFirst:cycleWithTailRest) = reverse $ dfs S.empty [firstNode]
+                cycle = (\(x,_,_) -> x) <$> fromVertex <$> (reverse $  cycleWithTailRest) -- takeWhile (/= cycleWithTailFirst)
+            in
+
+            let bfs :: Int -> Seq.Seq (S.Set Vertex, [Vertex], Vertex) -> [Vertex]
+                bfs i _ | 1000000 < i =
+                  let cycleWithTail@(cycleWithTailFirst:cycleWithTailRest) = reverse $ dfs S.empty [firstNode]
+                  in cycleWithTailFirst : (reverse $ (cycleWithTailFirst : takeWhile (/= cycleWithTailFirst) cycleWithTailRest))
+                bfs i (rest Seq.:|> (aset,apath,a)) | S.member a aset = (a:apath)
+                bfs i (rest Seq.:|> (aset,apath,a)) =
+                  let outs = Seq.fromList $ (\x -> (S.insert a aset, a:apath, x)) <$> filter (\x -> S.member x sccNodes) (graph ! a) in
+                  bfs (i + Seq.length outs) (outs <> rest)
+
+                cycleWithTail = reverse $ bfs 0 startState
+            in
+            NE.fromList $ map (\(x,_,_) -> x) $ map fromVertex $ reverse $
+              case cycleWithTail of
+                [a,b] | a == b -> [a]
+                a -> a
+
+          ms'' =
+            case msRest of
+              [] -> ms'
+              [_] -> ms'
+              _ ->
+                shortestCycle msRoot
+      in
       throwError
-        . errorMessage'' (fmap (sigSourceSpan . snd) ms')
-        $ CycleInModules (map (sigModuleName . snd) ms')
+        . errorMessage'' (fmap (sigSourceSpan . snd) ms'')
+        $ CycleInModules (map (sigModuleName . snd) ms'')
 
 moduleSignature :: Module -> ModuleSignature
 moduleSignature (Module ss _ mn ds _) = ModuleSignature ss mn (ordNub (mapMaybe usedModules ds))
