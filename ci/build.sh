e80dd4e99b96819371b54b9b77419265aee91a2c
drathier <github@fi.fo>
2025-07-22 16:58:47 +0200

    Try bumping CI

--- ci/build.sh
+++ ci/build.sh
@@ -25,15 +25,18 @@ set -ex
 
 # We test with --haddock because haddock generation can fail if there is invalid doc-comment syntax,
 # and these failures are very easy to miss otherwise.
-STACK="stack --no-terminal --haddock --jobs=2"
+STACK="stack --no-terminal --haddock --jobs=4"
 
-#STACK_OPTS="--test" # not running tests; we just want something built
-STACK_OPTS=""
+STACK_OPTS="--test"
 if [ "$CI_RELEASE" = "true" -o "$CI_PRERELEASE" = "true" ]
 then
   STACK_OPTS="$STACK_OPTS --flag=purescript:RELEASE"
 else
   STACK_OPTS="$STACK_OPTS --fast"
+fi
+if [ "$CI_STATIC" = "true" ]
+then
+  STACK_OPTS="$STACK_OPTS --flag=purescript:static"
 fi
 
 (echo "::endgroup::"; echo "::group::Set version number for build") 2>/dev/null
